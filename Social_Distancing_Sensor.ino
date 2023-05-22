8faebdd60c6128e3cabf189c1513a1e861119702
Dany Rashwan <61055337+dannirash@users.noreply.github.com>
2023-05-22 16:33:40 -0400

    Update Social_Distancing_Sensor.ino

--- Social_Distancing_Sensor.ino
+++ Social_Distancing_Sensor.ino
@@ -4,10 +4,6 @@ const int echo = 13;
 const int LED1 = 8;
 const int LED2 = 7;
 const int LED3 = 6;
-const int LED4 = 5;
-const int LED5 = 4;
-const int LED6 = 3;
-const int LED7 = 2;
 
 int duration = 0;
 int distance = 0;
@@ -20,10 +16,6 @@ void setup()
   pinMode(LED1 , OUTPUT);
   pinMode(LED2 , OUTPUT);
   pinMode(LED3 , OUTPUT);
-  pinMode(LED4 , OUTPUT);
-  pinMode(LED5 , OUTPUT);
-  pinMode(LED6 , OUTPUT);
-  pinMode(LED7 , OUTPUT);
   
   Serial.begin(9600);
 
@@ -49,6 +41,7 @@ void loop()
   {
     digitalWrite(LED1, LOW);
   }
+  
   if ( distance <= 14 )
   {
     digitalWrite(LED2, HIGH);
@@ -57,6 +50,7 @@ void loop()
   {
     digitalWrite(LED2, LOW);
   }
+  
   if ( distance <= 21 )
   {
     digitalWrite(LED3, HIGH);
@@ -64,37 +58,5 @@ void loop()
   else
   {
     digitalWrite(LED3, LOW);
-  }
-  if ( distance <= 28 )
-  {
-    digitalWrite(LED4, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED4, LOW);
-  }
-  if ( distance <= 35 )
-  {
-    digitalWrite(LED5, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED5, LOW);
-  }
-  if ( distance <= 42 )
-  {
-    digitalWrite(LED6, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED6, LOW);
-  }
-  if ( distance <= 49 )
-  {
-    digitalWrite(LED7, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED7, LOW);
   }
 }
